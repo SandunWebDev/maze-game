22f92fc446ae1deefe1751e1327d78afa121264c
SandunWebDev <sandunwebdev@gmail.com>
2019-02-05 10:08:51 +0530

    Optimied sound file size.

--- src/componenets/widgets/MazeBoard/helpers/musicHandler.js
+++ src/componenets/widgets/MazeBoard/helpers/musicHandler.js
@@ -5,12 +5,12 @@ export const backgroundMusicPlayer = new Howl({
   src: ["../assets/sounds/gameBackgroundMusic.mp3"],
   autoplay: false,
   loop: true,
-  volume: 0.4
+  volume: 0.8
 });
 
 export const walkSound = new Howl({
   src: ["../assets/sounds/walk.mp3"],
-  volume: 0.5,
+  volume: 0.8,
   sprite: {
     walk: [0, 500]
   }
